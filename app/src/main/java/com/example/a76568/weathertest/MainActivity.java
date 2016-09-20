5f2e8df0fbcc9c500373d498761baefe4b47983c
a15082671703 <137840844@qq.com>
2016-09-20 21:41:48 +0800

    init product

---
+++ app/src/main/java/com/example/a76568/weathertest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a76568.weathertest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 76568 on 2016/9/20 0020.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_infos);
+    }
+}
